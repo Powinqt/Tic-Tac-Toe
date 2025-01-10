bba320cb419c10f425893050ba98a56f751475d9
Powinqt <139686187+Powinqt@users.noreply.github.com>
2025-01-10 13:22:41 +0700

    Add files via upload
    Add Files

---
+++ Tic-Tac-Toe/TicTacToe/TTT/PlayButton.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using TicTacToe;
+
+namespace TTT
+{
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        // Button click event handler to open the main game window
+        private void PVP_Click(object sender, RoutedEventArgs e)
+        {
+            // Create and show the main game window
+            var pvp = new PVP();
+            pvp.Show();
+
+            // Close the splash screen window
+            this.Close();
+        }
+
+        private void PVC_Click(object sender, RoutedEventArgs e)
+        {
+            // Create and show the main game window
+            var pvc = new PVC();
+            pvc.Show();
+
+            // Close the splash screen window
+            this.Close();
+        }
+    }
+}
