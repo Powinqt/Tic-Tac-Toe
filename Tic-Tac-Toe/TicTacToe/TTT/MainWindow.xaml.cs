a89f3e1cc069fd3b0797fd7000672fde046b8f66
Powinqt <139686187+Powinqt@users.noreply.github.com>
2025-01-10 21:57:30 +0700

    Add files via upload

---
+++ Tic-Tac-Toe/TicTacToe/TTT/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace TicTacToe
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PVP_Click(object sender, RoutedEventArgs e)
+        {
+            // Start the game (open PVP or PVC window)
+            PVP pvpWindow = new PVP();
+            pvpWindow.Show();
+            this.Close(); // Close the MainMenu window
+        }
+
+        private void PVC_Click(object sender, RoutedEventArgs e)
+        {
+            // Start the game (open PVP or PVC window)
+            PVC pvcWindow = new PVC();
+            pvcWindow.Show();
+            this.Close(); // Close the MainMenu window
+        }
+    }
+}
